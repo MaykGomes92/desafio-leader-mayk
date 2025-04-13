db416241fad898e0a58f7fca3fcde38ef28b7d90
Mayk Gomes <maykcontato98@hotmail.com>
2025-04-12 23:17:13 -0300

    Theme no LocalStorage

--- src/Hook/Theme.jsx
+++ src/Hook/Theme.jsx
@@ -6,14 +6,24 @@ import { Button } from '@/components/ui/button'
 export default function Theme() {
 
   const [iconTheme, setIconTheme] = React.useState(false)
-
   const element = document.documentElement;
+  const [themeStorage, setThemeStorage] = React.useState('')
+
+  React.useLayoutEffect(() => {
+    setThemeStorage(window.localStorage.getItem('theme'))
+    if(themeStorage !== ''){
+      element.classList.add(themeStorage)
+    }
+  },[themeStorage])
+  
   function toogleTheme() {
     setIconTheme(!iconTheme)
     if (element.classList[0] == 'dark') {
       element.classList.remove('dark')
+      window.localStorage.setItem('theme','')
     } else {
       element.classList.add('dark')
+      window.localStorage.setItem('theme','dark')
     }
   }
 
